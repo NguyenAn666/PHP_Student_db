70f811c542181f54c8c5e4c5734f92e498737478
NguyenAn666 <97713637+NguyenAn666@users.noreply.github.com>
2022-09-12 10:34:23 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$dataHost = 'localhost';
+$dataName = 'php_student';
+$dataUserName = 'root' ;
+$dataPassword = '';
+$mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassword,$dataName);
+?>
